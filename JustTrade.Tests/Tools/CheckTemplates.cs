4e9c247a08107ae97e099bc884ed6f625d9be9d7
Sergey Ratkin <greshnik-sv@yandex.ru>
2015-05-21 21:57:28 +0300

    Add to database template file variable tags. Create text for check database template file.

---
+++ JustTrade.Tests/Tools/CheckTemplates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JastTrade.Tests
+{
+	[TestFixture]
+	public class CheckTemplates
+	{
+		private bool ExistTemplatePropesties(string fileContent)
+		{
+			if (fileContent.Contains ("[host]") ||
+				fileContent.Contains ("[dabatase]") ||
+				fileContent.Contains ("[user]") ||
+				fileContent.Contains ("[password]")) {
+				return true;
+			}
+			return false;
+		}
+
+		[Test]
+		public void CheckFileContext()
+		{
+			StringBuilder builder = new StringBuilder ();
+			var templates = Directory.GetFiles (@".\ConfigurationTemplates\");
+			foreach (var template in templates) 
+			{
+				if (!ExistTemplatePropesties (File.ReadAllText (template))) {
+					builder.AppendFormat ("Not found template variable block in file:{0}\n",Path.GetFileName(template));
+				}
+			}
+			Assert.IsTrue (builder.Length <= 0, builder.ToString ());
+		}
+
+	}
+}
+
--- JustTrade/Controllers/AdminController.cs
+++ JustTrade/Controllers/AdminController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace JastTrade.Controllers
 {
 	public class AdminController : Controller
     {
+		private void ReplaceTemplateVariable(string host, string database, string user, 
+			string password, ref string fileContent)
+		{
+			string file = fileContent;
+			file = file.Replace ("[host]", host);
+			file = file.Replace ("[database]", database);
+			file = file.Replace ("[user]", user);
+			file = file.Replace ("[password]", password);
+		}
+
+
+
         public ActionResult Index()
         {
             return View ();
@@ -15,8 +28,23 @@ namespace JastTrade.Controllers
 
 		public ActionResult Database()
 		{
-
 			return PartialView ();
+		}
+
+		[HttpGet]
+		public object GetDatabaseTypeList(){
+			return Json (System.IO.Directory.GetFiles (@".\bin\ConfigurationTemplates\").Select(x=>Path.GetFileName(x)), JsonRequestBehavior.AllowGet);
+		}
+
+		[HttpGet]
+		public object CreateDatabase(string databaseType, string host, string database, string user, string password){
+			string file = string.Format (@".\ConfigurationTemplates\{0}.xml", databaseType);
+			if (!System.IO.File.Exists (file)) {
+				throw new FileNotFoundException (file);
+			}
+			string fileContent = System.IO.File.ReadAllText (file);
+			ReplaceTemplateVariable (host,database,user,password, ref fileContent);
+			return new { a = "asdsaf" };
 		}
 
     }
